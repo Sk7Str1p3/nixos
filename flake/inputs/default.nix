51be18d5175472efe9e9d413ad45e9842b6e6b00
Sk7Str1p3 <finenkofedor@gmail.com>
2026-05-02 14:27:20 +0300

    fix(docs): fix `unclosed HTML tag` warning

--- flake/inputs/default.nix
+++ flake/inputs/default.nix
@@ -10,8 +10,8 @@
   > `flake.nix` is generated via [flake-file](https://github.com/vic/flake-file). Then adding new input,
   > remember running `nix run .#write-flake` to apply changes!
   >
-  > In next chapters, you'll see attributes of type `flake.inputs.<class>.<input>`.
-  > Then accessing inputs in repl, remove <class>, so instead of calling e.g. `flake.inputs.core.nixpkgs`,
+  > In next chapters, you'll see attributes of type `flake.inputs.{class}.{input}`.
+  > Then accessing inputs in repl, remove {class}, so instead of calling e.g. `flake.inputs.core.nixpkgs`,
   > you call `flake.inputs.nixpkgs`.
 */
 import ./core.nix
